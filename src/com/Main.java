6f57473c0d2a9a8bcc3c37c555a39e6a97d93b56
ivailo <ivailo.s.stanev@gmail.com>
2018-08-30 22:36:15 +0300

    addded JUnit5

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,10 +4,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public Main() {
+    private String field1 = "";
+    public Main(String name) {
+        this.field1 = name;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
     }
 
     public static void main(String[] args){
+
+
         System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.google.com");
